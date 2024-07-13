a6bfb62675217d6da0bc87b10bf139b7c37617aa
falgunidable <falgunidable@gmail.com>
2024-07-13 15:33:09 +0530

    checking for updates

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          <h1>Falguni Dable</h1>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
